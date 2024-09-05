aba0f34754fc69b02245192c19beb78bfef0c205
Aiden Powers <powersajbusiness@protonmail.com>
2024-09-04 19:49:49 -0700

    Cleaner playerSpeed.java

--- src/client/java/powersaj/dungeonsfeatures/function/playerSpeed.java
+++ src/client/java/powersaj/dungeonsfeatures/function/playerSpeed.java
@@ -8,27 +8,23 @@ import powersaj.dungeonsfeatures.DungeonsFeaturesValues;
 import java.util.Objects;
 
 public class playerSpeed {
+    /**
+     * @param player The player object to change the speed of
+     * @param speed The speed to set the player to
+     * @param type The type of speed to change (flight or walk)
+     */
     public static void changePlayerClientSpeed(PlayerEntity player, float speed, String type) {
-        if (type == null) {
-            if (player.getAbilities().flying) {
-                type = "flight";
-            } else {
-                type = "walk";
-            }
-        }
-
-        if (type.equals("flight")) {
-            player.getAbilities().setFlySpeed(0.05f * speed);
-        } else {
-            Objects.requireNonNull(player.getAttributeInstance(EntityAttributes.GENERIC_MOVEMENT_SPEED)).setBaseValue(0.1f * speed);
-        }
+        if (type == null) if (player.getAbilities().flying) type = "flight"; else type = "walk";
+        if (type.equals("flight")) player.getAbilities().setFlySpeed(0.05f * speed);
+        Objects.requireNonNull(player.getAttributeInstance(EntityAttributes.GENERIC_MOVEMENT_SPEED)).setBaseValue(0.1f * speed);
         player.sendAbilitiesUpdate();
     }
-
+    /**
+     * @param client The MinecraftClient object to update the player speed of.
+     * @see DungeonsFeaturesValues#playerSpeed
+     */
     public static void updatePlayerSpeed(MinecraftClient client) {
         PlayerEntity player = client.player;
-        if (player != null) {
-            changePlayerClientSpeed(player, DungeonsFeaturesValues.playerSpeed, null);
-        }
+        if (player != null) changePlayerClientSpeed(player, DungeonsFeaturesValues.playerSpeed, null);
     }
 }
