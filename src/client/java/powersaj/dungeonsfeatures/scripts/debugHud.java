8ccc4b4dd5d56d1cd45591c3c5cb1439f4c05204
Aiden Powers <powersajbusiness@protonmail.com>
2024-09-04 19:58:23 -0700

    Extracted debug hud from PowersajsDungeonsFeaturesClient.java to debugHud.java

---
+++ src/client/java/powersaj/dungeonsfeatures/scripts/debugHud.java
@@ -0,0 +1,19 @@
+package powersaj.dungeonsfeatures.scripts;
+
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.DrawContext;
+import net.minecraft.client.render.RenderTickCounter;
+import net.minecraft.text.Text;
+import powersaj.dungeonsfeatures.DungeonsFeaturesValues;
+
+import java.util.Objects;
+
+public class debugHud {
+    public static void debugHudRender(DrawContext context, RenderTickCounter tickDeltaManager) {
+        MinecraftClient client = MinecraftClient.getInstance();
+        context.drawText(client.textRenderer, Text.of("Powersaj's Dungeons Features Debug"), 10, 10, 0xFFFFFFFF, true);
+        context.drawText(client.textRenderer, Text.of("DungeonsFeaturesValues.entityStepHeight: " + Objects.requireNonNull(client.getCameraEntity()).getStepHeight()), 10, 20, 0xFFFFFFFF, true);
+        context.drawText(client.textRenderer, Text.of("DungeonsFeaturesValues.playerSpeed: " + DungeonsFeaturesValues.playerSpeed), 10, 30, 0xFFFFFFFF, true);
+
+    }
+}
